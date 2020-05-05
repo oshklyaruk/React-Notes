a3b853d9d4f12d4934a0c0c611f996fa3987ec81
Olga Shklyaruk <oshklyaruk@Olgas-MacBook-Pro.local>
2020-05-05 17:14:04 +0300

    refactoring

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import './Note.sass';
 import Button from '../Button/Button';
 
-const Note = ({ note, onDeleteNote }) => {
-  const { id, title, description, date } = note;
+const Note = ({note, onDeleteNote}) => {
+  const {id, title, description, date} = note;
 
   return (
-      <div key={id} className="note">
-        <h4>{title}</h4>
-        <span className="date">{`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}</span>
-        <span className="description">{description}</span>
-        <Button style={{ backgroundColor: "#de5151"}} onClick={() => onDeleteNote(id)}>Delete note</Button>
-      </div>
+    <div key={id} className="note">
+      <h4>{title}</h4>
+      <span className="date">{`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}</span>
+      <span className="description">{description}</span>
+      <Button style={{backgroundColor: "#de5151"}} onClick={() => onDeleteNote(id)}>Delete note</Button>
+    </div>
   )
 }
 
