cbd83d0611e8a916c8d5ff9d270483fb2039066a
Olga Shklyaruk <oshklyaruk@Olgas-MacBook-Pro.local>
2020-05-05 12:41:27 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.sass';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
